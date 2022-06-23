edc0b3c72d8fbbd225dfe7147cfd3bbc3b27e3df
Carolyn Van Slyck <me@carolynvanslyck.com>
2022-06-23 12:53:37 -0500

    Use an image optimized for twitter cards
    * The image I was trying to show (our website background) is too large and
    doesn't have the right aspect ratio. This new image is cropped to 2:1 so
    that twitter is willing to display it for the card image.
    
    * Hard-code the domain for the production site
    
    Signed-off-by: Carolyn Van Slyck <me@carolynvanslyck.com>

--- magefile.go
+++ magefile.go
@@ -102,7 +102,7 @@ func Hugo() error {
 func Deploy() error {
 	mg.Deps(docsy, syncGoMod)
 
-	return shx.RunV("hugo", "-s", "website", "--debug", "--verbose", "-b", getBaseUrl())
+	return shx.RunV("hugo", "-s", "website", "--debug", "--verbose", "-b", "https://contribute.cncf.io/")
 }
 
 // Deploy branch builds the website, including future dated and draft posts
